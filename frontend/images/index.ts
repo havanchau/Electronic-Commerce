54ce5d0d2a78ef702f0f2a8891eb6af3b713434c
havanchau <havanchau@hanbiro.vn>
2024-10-09 15:27:24 +0700

    [chore] edit homepage

--- frontend/images/index.ts
+++ frontend/images/index.ts
@@ -1,5 +1,7 @@
-import background from './scandinavian-interior-mockup-wall-decal-background 1.png'
+import background from './scandinavian-interior-mockup-wall-decal-background 1.png';
+import logo from './Meubel House_Logos-05.png';
 
 export const Images = {
     background: background,
+    logo: logo,
 }
--- frontend/lang/vi/lang.ts
+++ frontend/lang/vi/lang.ts
@@ -8,4 +8,9 @@ export const lang = {
     loginFail: 'Đăng nhập thất bại, Vui lòng thử lại',
     registerSuccess: 'Đăng kí thành công!',
     registerFail: 'Đăng kí thất bại, Vui lòng thử lại',
+    buyNow: 'Mua ngay',
+    newArrival: 'Hàng mới về',
+    discoverCollection: 'Khám phá bộ sưu tập mới của chúng tôi',
+    introduceContent: 'Khám phá dòng sản phẩm thân thiện với môi trường của chúng tôi được thiết kế để giúp bạn theo đuổi lối sống bền vững.',
+    
 }
