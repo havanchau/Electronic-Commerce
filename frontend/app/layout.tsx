eafd7c9bd92e69ecca774dceaab167610056ebe4
havanchau <havanchau@hanbiro.vn>
2024-10-09 08:45:06 +0700

    [feat] add spinner when loading routing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,16 @@
-import type { Metadata } from "next";
+'use client';
+
 import localFont from "next/font/local";
-import "./globals.css";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import LoadingSpinner from "@/app/components/LoadingSpinner/LoadingSpinner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -18,12 +22,41 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    setLoading(true);
+    
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 700);
+
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {loading ? (
+          <div style={{ 
+            position: 'fixed', 
+            top: 0, 
+            left: 0, 
+            width: '100%', 
+            height: '100%', 
+            backgroundColor: 'white',
+            zIndex: 9999,
+            display: 'flex',
+            justifyContent: 'center', 
+            alignItems: 'center' 
+          }}>
+            <LoadingSpinner />
+          </div>
+        ) : (
+          children
+        )}
       </body>
     </html>
   );
